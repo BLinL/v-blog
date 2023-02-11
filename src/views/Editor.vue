6b2a693d5886432da2dc2f8b4444e08a22628944
baolin.liu <1073842723@qq.com>
2023-02-11 15:06:17 +0800

    feat: 编辑页面离开提示

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="m-2">
     <h1 class="text-xl mb-8">编辑</h1>
-    <div class="blog-tittle"><input class="tittle-input" v-model="tittle" placeholder="请输入文章标题..."/> 标题</div>
+    <div class="blog-tittle">
+      <input
+        class="tittle-input"
+        v-model="tittle"
+        placeholder="请输入文章标题..."
+      />
+      标题
+    </div>
     <h1 class="text-xl mb-2">内容</h1>
     <v-md-editor v-model="txt" height="600px" @save="save"></v-md-editor>
   </div>
@@ -9,8 +16,9 @@
 
 <script>
 import { ref } from "vue";
+import { onBeforeRouteLeave } from "vue-router";
 import request from "../utils/request.js";
-import swal from 'sweetalert'
+import swal from "sweetalert";
 
 export default {
   setup() {
@@ -24,28 +32,39 @@ export default {
         author: "bliu",
         createTime: new Date(),
       };
-      if (text.length==0) {
-        swal("请输入一些内容","","error")
-        return
+      if (text.length == 0) {
+        swal("请输入一些内容", "", "error");
+        return;
       }
-      if(tittle.value.length == 0) {
-        swal("请输入文章标题","","error")
-        return
+      if (tittle.value.length == 0) {
+        swal("请输入文章标题", "", "error");
+        return;
       }
       request
-        .post("/api/blog/save",blogData)
+        .post("/api/blog/save", blogData)
         .then(function (response) {
           console.log(response);
           if (response.data.code != 200) {
-            swal("非常抱歉，保存出现了一点问题","","error")
+            swal("非常抱歉，保存出现了一点问题", "", "error");
           } else {
-            swal("文章发布成功啦！","","success")
+            swal("文章发布成功啦！", "", "success");
           }
         })
         .catch(function (error) {
           console.log(error);
         });
     };
+
+    onBeforeRouteLeave(async (to, from) => {
+      const answer = await swal({
+        title: "Are you sure?",
+        text: "您的文章可能还未保存，确认离开吗",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      if(!answer) return false
+    });
     return { tittle, txt, save };
   },
 };
@@ -56,13 +75,13 @@ export default {
   outline: none;
   border: 2px solid #9595f3;
 }
-.tittle-input{
+.tittle-input {
   background: #fff;
   border: 2px solid #eee;
   padding: 10px;
-  border-radius:10px;
+  border-radius: 10px;
   box-shadow: none;
-  transition: width .3s;
+  transition: width 0.3s;
 }
 .blog-tittle {
   margin: 30px;
